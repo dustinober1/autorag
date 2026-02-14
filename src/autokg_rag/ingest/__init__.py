f5b3f8fbe65ecdb0a1b63bc39a06e7947d670397
dustinober1 <dober.code@gmail.com>
2026-02-14 16:50:59 -0500

    feat(phase1): update __init__.py for PMBOK-aware ingestion

--- src/autokg_rag/ingest/__init__.py
+++ src/autokg_rag/ingest/__init__.py
@@ -1,5 +1,16 @@
-"""Ingest pipeline package."""
+"""Ingest pipeline package with PMBOK-aware parsing."""
 
 from autokg_rag.ingest.pipeline import run_ingest_pipeline, run_smoke_pipeline
+from autokg_rag.ingest.pmbok_toc_parser import PmbokTocParser, TocEntry, load_pmbok_toc
+from autokg_rag.ingest.table_extractor import ExtractedTable, extract_tables_from_pdf, table_to_markdown
 
-__all__ = ["run_ingest_pipeline", "run_smoke_pipeline"]
+__all__ = [
+    "run_ingest_pipeline",
+    "run_smoke_pipeline",
+    "PmbokTocParser",
+    "TocEntry",
+    "load_pmbok_toc",
+    "ExtractedTable",
+    "extract_tables_from_pdf",
+    "table_to_markdown",
+]
