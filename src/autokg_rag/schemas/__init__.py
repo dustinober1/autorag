3c06e43d455aa740fc1e3ec22c60e10d79f16ae2
dustinober1 <dober.code@gmail.com>
2026-02-13 20:59:16 -0500

    chore(m3): export kg schemas

--- src/autokg_rag/schemas/__init__.py
+++ src/autokg_rag/schemas/__init__.py
@@ -3,21 +3,27 @@
 from autokg_rag.schemas.provenance import Citation
 from autokg_rag.schemas.records import (
     AnswerRecord,
+    ChunkMentionRecord,
     ChunkRecord,
     DocumentManifestRecord,
     DocumentRecord,
     EmbeddingMetaRecord,
+    KGEdgeRecord,
+    KGNodeRecord,
     PageRecord,
     RetrievalHitRecord,
 )
 
 __all__ = [
     "AnswerRecord",
+    "ChunkMentionRecord",
     "ChunkRecord",
     "Citation",
     "DocumentManifestRecord",
     "DocumentRecord",
     "EmbeddingMetaRecord",
+    "KGEdgeRecord",
+    "KGNodeRecord",
     "PageRecord",
     "RetrievalHitRecord",
 ]
